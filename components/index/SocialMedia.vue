8f23a5aad0b30314b934b353f34d725d31c765e6
dendydandees <dendydharmawanq@gmail.com>
2021-10-14 17:45:33 +0700

    [FIX] social media layout for xl devices

--- components/index/SocialMedia.vue
+++ components/index/SocialMedia.vue
@@ -6,8 +6,9 @@
     <div
       class="absolute left-0 right-0 top-0 bottom-0 mx-auto my-auto h-20 z-0 filter blur-3xl animate-pulse bg-gradient-to-r from-purple-800 dark:from-purple-600 to-yellow-800 dark:to-yellow-600 w-full md:w-1/3"
     />
+
     <div
-      class="relative flex flex-wrap justify-center items-center mx-auto z-10 py-6 w-full md:w-8/12 lg:w-6/12 xl:w-4/12"
+      class="relative flex flex-wrap justify-center items-center mx-auto z-10 py-6 w-full md:w-8/12 lg:w-6/12 xl:w-3/12"
     >
       <a
         v-for="(socialMedia, index) in socialMediaList"
